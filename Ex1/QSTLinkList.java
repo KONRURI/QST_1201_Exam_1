def4b8786b80b345c72661804dc49200e3978f6b
KONRURI <505076933@qq.com>
2016-12-07 10:21:20 +0800

    Add files via upload

---
+++ Ex1/QSTLinkList.java
@@ -0,0 +1,65 @@
+package HadoopProject;
+
+import java.util.Stack;
+
+public class QSTLinkList 
+{
+	public static class Node{
+		public int value;
+		public Node next = null;
+		Node(int value){
+			this.value = value;
+		}
+		Node(int value, Node next){
+			this.value = value;
+			this.next = next;
+		}
+		public void setValue(int value){
+			this.value = value;
+		}
+	}
+	private static void printLinkList(Node head) {
+		Stack<Node> stack = new Stack<Node>();
+		Node node = head;
+		Node cnode=head;
+		
+		while(node!=null){
+			stack.push(node);
+			node = node.next;
+		}
+		int i = stack.size();
+		while(i>0){
+			System.out.print(cnode.value);
+			cnode = cnode.next;
+			
+				System.out.print("->");
+		
+			i--;
+		}
+		
+		
+	}
+  
+  public static void main( String[] args ){
+    int[] arr = {1,3,5,7,2,4};
+    Node head = createLinkList(arr);
+    printLinkList(head);
+  }
+	private static Node createLinkList(int[] arr) {
+		// TODO Auto-generated method stub
+		Node[] linkArr = new Node[arr.length];
+		for (int i=0; i<arr.length; i++){
+			linkArr[i] = new Node(arr[i]);
+		}
+		for (int i=0; i<arr.length; i++){
+			linkArr[i].setValue(arr[i]);
+			if (i == arr.length - 1){
+				linkArr[i].next = null;
+			}
+			else{
+				linkArr[i].next = linkArr[i+1];
+			}
+		}
+		return linkArr[0];
+	}
+}
